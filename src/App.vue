31edc6e8ba47ca02cc9ce33346cd3266a92ad958
cxl <3288270459@qq.com>
2022-10-31 14:49:04 +0800

    引入vant

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,8 @@
   <div id="app">
     <h1>黑马头条</h1>
      <!-- 路由出口 -->
+      <i class="toutiao toutiao-lishi"></i>
+      <van-button type="info">信息按钮</van-button>
     <router-view/>
    
   </div>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 导入less文件
 import './styles/index.less'
-
+//导入vant  按钮等
+import { Button } from 'vant';
 Vue.config.productionTip = false
+
+
+Vue.use(Vant)
+Vue.use(Button)
 
 new Vue({
   router,
