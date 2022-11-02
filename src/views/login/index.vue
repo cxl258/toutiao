a5f12765856f4638205fcc42ee1b8fee9170fafb
cxl <3288270459@qq.com>
2022-11-02 10:03:02 +0800

    登录页面第二次修改

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="login-container">
-       <van-nav-bar title="登录"  class="page-nav-bar"/>
-     <van-form>
-       <van-field  name="用户名" placeholder="输入手机号"/>
-       <van-field  name="密码" placeholder="输入密码"/>
-       <div style="margin:16px;">
-          <van-button round block  type="info" native-type="submit"> 提交</van-button>
-       </div>
-     </van-form>
+    <van-nav-bar title="登录" class="page-nav-bar" />
 
+    <van-form>
+      <van-field name="用户名" placeholder="输入手机号">
+        <i slot="left-icon" class="toutiao toutiao-shouji"></i>
+      </van-field>
+
+      <van-field name="密码" placeholder="输入密码">
+        <i slot="left-icon" class="toutiao toutiao-yanzhengma"></i>
+        <template #button>
+          <van-button class="send-sms-btn" round size="small" type="default"
+            >发送验证码</van-button
+          >
+        </template>
+      </van-field>
+
+      <div style="margin: 16px">
+        <van-button round block type="info" native-type="submit">
+          提交</van-button
+        >
+      </div>
+    </van-form>
   </div>
 </template>
 
@@ -33,18 +46,16 @@ export default {
 </script>
 
 <style scoped lang="less">
-
 .login-container {
-
   .toutiao {
     font-size: 37px;
   }
   .send-sms-btn {
-    width: 152px;
+    width: 160px;
     height: 46px;
     line-height: 46px;
     background-color: #ededed;
-    font-size: 22px;
+    font-size: 14px;
     color: #666;
   }
   .login-btn-wrap {
